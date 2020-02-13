ff726a03ecc85de39700ba158007066f12a7d113
Daniel Chapman <danielc@my.ccsu.edu>
2020-02-13 17:09:41 -0500

    Equipping works

--- src/investigation.js
+++ src/investigation.js
@@ -1,6 +1,48 @@
 let equip = [0,0,0,0,0,0,0,0];
-let change = ['*','*','*','','*','','*',''];
-let numChange = [2,2,2,2,2,2,2,2];
+let change = [
+    {
+        symbol: '',
+        slot: 'mainhand',
+        reference: 'mainhands'
+    },
+    {
+        symbol: '',
+        slot: 'offhand',
+        reference: 'offhands'
+    },
+    {
+        symbol: '',
+        slot: 'head',
+        reference: 'heads'
+    },
+    {
+        symbol: '',
+        slot: 'body',
+        reference: 'bodies'
+    },
+    {
+        symbol: '',
+        slot: 'necklace',
+        reference: 'necklaces'
+    },
+    {
+        symbol: '',
+        slot: 'ring',
+        reference: 'rings'
+    },
+    {
+        symbol: '*',
+        slot: 'accessory',
+        reference: 'accessories'
+    },
+    {
+        symbol: '',
+        slot: 'mount',
+        reference: 'mounts'
+    },
+
+];
+let numChange;
 
 let sortedEquipment =  {
     mainhands: [],
@@ -12,6 +54,18 @@ let sortedEquipment =  {
     accessories: [],
     pets: []
 };
+
+var equipped = {
+    mainhand: {},
+    offhand: {},
+    head: {},
+    body: {},
+    necklace: {},
+    ring: {},
+    accessory: {},
+    pet: {},
+    mount: {}
+  };
 
 
 Object.keys(equipment).forEach( (x) => {
@@ -45,29 +99,33 @@ Object.keys(equipment).forEach( (x) => {
 
 numChange = [sortedEquipment.mainhands.length,sortedEquipment.offhands.length,sortedEquipment.heads.length,sortedEquipment.bodies.length,sortedEquipment.necklaces.length,sortedEquipment.rings.length,sortedEquipment.accessories.length,16];
 
-numOptions;
-
-let endingIndex = equip.length-1;
-let startIndex = 0;
-
-function startRecurve() {
-    recurveIncrement(startIndex);
-}
-
-function recurveIncrement(index) {
+function recurveIncrement(index, equipped) {
     let i = index;
+    let r = equipped;
+    console.log(r);
 
     if (i >= equip.length-1) {
+        console.log(r)
         //This is where calculations would be.
         //Would set a maximum of specified result
         return ; 
     }
-    if (change[i] === '*') {
-        //skip changing this equip
+    if (change[i].symbol === '*') {
+        //only change equipment on this stage
         let t = index + 1;
         for(let x = 0; x < numChange[i]; x++) {
             equip[i]+=1;
-            recurveIncrement(t);
+            try {
+                r[change[i].slot] = sortedEquipment[change[i].reference][x];
+            } catch (error) {
+                console.log({
+                    i: i,
+                    x: x,
+                });
+                console.log(sortedEquipment[change[i].reference][x]);
+                console.log(r[change[i].slot]);
+            }
+            recurveIncrement(t, r);
         }
     } else {
         i+=1;
